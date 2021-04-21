94a0a44209c7d41d2d9dd5e8238ae2b7366ca0c3
Caetano Borges <cbcaetanoborges@gmail.com>
2021-04-21 08:42:28 +0100

    ---

---
+++ inicio.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conta</title>
+</head>
+<style>
+    .cabecalho{width: 100%;height:60px;border-bottom: 1px solid #eaeaea;}
+    .logotipo{width: 80px;float: left;margin-left: 10px;}
+    .botao-img{width: 40px;float: right;margin:5px 10px 0 0;}
+    .avatar{width: 40px;float: right;margin:5px 10px 0 0;}
+</style>
+<body>
+    <div class="cabecalho">
+        <img src="logo.png" class="logotipo">
+        <img src="icones/default.png" class="avatar">
+        <img src="icones/apps.png" class="botao-img">
+        <img src="icones/ajuda.png" class="botao-img">
+    </div>
+    <div class="lado">
+        <ul>
+            <a href="">
+                <li>Inicio</li>
+            </a>
+            <a href="">
+                <li>Inicio</li>
+            </a>
+            <a href="">
+                <li>Inicio</li>
+            </a>
+            <a href="">
+                <li>Inicio</li>
+            </a>
+        </ul>
+    </div>
+    <div class="corpo">
+        
+    </div>
+</body>
+</html>
