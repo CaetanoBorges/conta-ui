80626b3104b922add0752a2eb3db92c6c9629df9
Caetano Borges <cbcaetanoborges@gmail.com>
2021-04-22 15:22:05 +0100

    ---

--- inicio.php
+++ inicio.php
@@ -4,188 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="one.css">
     <title>Conta</title>
 </head>
-<style>
-* {
-    margin: 0;
-    padding: 0;
-}
-
-.cabecalho {
-    width: 100%;
-    height: 60px;
-    border-bottom: 1px solid #eaeaea;
-}
-
-.logotipo {
-    width: 80px;
-    float: left;
-    margin-left: 10px;
-}
-
-.botao-img {
-    width: 25px;
-    float: right;
-    margin: 18px 20px 0 0;
-}
-
-.avatar {
-    width: 40px;
-    float: right;
-    margin: 11px 10px 0 0;
-    border-radius: 50%;
-}
-
-.lado {
-    width: 250px;
-    float: left;
-    margin-top: 20px;
-}
-
-.lado ul {
-    list-style: none;
-    width: 100%
-}
-
-.lado ul a {
-    text-decoration: none;
-    color: #7a7a7a;
-    font-weight: 600;
-    text-align: left;
-}
-
-.lado ul li:hover {
-    background: rgba(0, 0, 0, .1)
-}
-
-.lado ul li {
-    width: 100%;
-    padding: 5px;
-    display: flex;
-    align-items: center;
-    border-radius: 0 5px 5px 0
-}
-
-.lado ul li img {
-    width: 25px;
-    margin: 0 10px 0 20px;
-}
-
-.linha-divisoria {
-    width: 100%;
-    height: 1px;
-    background: #eaeaea
-}
-
-.activo {
-    background: rgba(0, 0, 255, .1);
-    color: rgba(0, 0, 100, .6)
-}
-
-.activo:hover {
-    background: rgba(0, 0, 255, .1) !important;
-}
-
-
-.corpo {
-    width: 700px;
-    display: block;
-    margin: 20px auto;
-}
-
-.corpo-topo {
-    width: 100%;
-    margin-bottom: 10px;
-}
-
-.corpo-topo img {
-    width: 100px;
-    display: block;
-    margin: 0 auto 5px auto;
-}
-
-.corpo-topo h2 {
-    text-align: center;
-}
-
-.corpo-topo p {
-    text-align: center;
-}
-
-.dividir-corpo {
-    width: 335px;
-}
-
-.card-inicio {
-    width: 100%;
-    height: 300px;
-    display: block;
-    margin: 30px 0;
-    position: relative;
-    border: 1px solid #cccccc;
-    border-radius: 10px;
-}
-
-
-.direita {
-    float: right
-}
-
-.esquerda {
-    float: left
-}
-
-.limpa {
-    width: 100%;
-    display: block;
-    clear: both;
-    height: 10px
-}
-
-.escritas-card {
-    width: 52%;
-    padding: 5%;
-    float: left;
-}
-
-.escritas-card h3 {
-    color: black;
-}
-
-.escritas-card p {
-    color: rgba(0, 0, 0, .6);
-}
-
-.imagem-card {
-    width: 35%;
-    float: right;
-    margin-top: 75px;
-}
-
-.imagem-card img {
-    width: 90%;
-    display: block;
-    margin: 0 auto;
-}
-
-.acao-card {
-    width: 90%;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    padding: 5%;
-    border-top: 1px solid #cccccc;
-    border-radius: 0 0 10px 10px;
-    color: rgba(0, 0, 100, .8);
-    font-weight: 600;
-}
-
-.acao-card:hover {
-    background: #eaeaea;
-
-}
-</style>
 
 <body>
     <div class="cabecalho">
@@ -219,7 +40,7 @@
     <div class="corpo">
         <div class="corpo-topo">
             <img src="icones/default.png">
-            <h2>Olá, Nome Usuario</h2>
+            <p class="p-grande">Olá, Nome Usuario</p>
             <p>Faça a gestão das suas informações e da segurança para utilizar os serviços www da forma mais adequada
                 para si.</p>
         </div>
@@ -230,7 +51,7 @@
             <a href="">
                 <div class="card-inicio">
                     <div class="escritas-card esquerda">
-                        <h3>Tema do que s´ra abordado</h3> <br>
+                        <p class="p-medio"> Dados pessoais </p><br>
                         <p>escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt
                             escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt
                             escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt</p>
@@ -248,7 +69,7 @@
             <a href="">
                 <div class="card-inicio">
                     <div class="escritas-card esquerda">
-                        <h3>Tema do que s´ra abordado</h3> <br>
+                        <p class="p-medio"> Dados pessoais </p><br>
                         <p>escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt
                             escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt
                             escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt</p>
@@ -270,7 +91,7 @@
             <a href="">
                 <div class="card-inicio">
                     <div class="escritas-card esquerda">
-                        <h3>Tema do que s´ra abordado</h3> <br>
+                        <p class="p-medio"> Dados pessoais </p><br>
                         <p>escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt
                             escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt
                             escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt</p>
@@ -288,7 +109,7 @@
             <a href="">
                 <div class="card-inicio">
                     <div class="escritas-card esquerda">
-                        <h3>Tema do que s´ra abordado</h3> <br>
+                        <p class="p-medio"> Dados pessoais </p><br>
                         <p>escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt
                             escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt
                             escritas do card e mais coisas aquin fjrhtvr tjrh tem tertjrtmjrt</p>
