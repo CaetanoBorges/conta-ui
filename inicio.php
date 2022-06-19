99a28efca78bfa3210de84ceb2ebf24aad75d214
Caetano Borges <cbcaetanoborges@gmail.com>
2022-06-19 22:00:21 +0100

    go

--- inicio.php
+++ inicio.php
@@ -1,4 +1,14 @@
 <!DOCTYPE html>
+<script>
+    var token = (window.location.href).split("ftpadbc=")[1];
+    if(token != null || token != ""){
+        localStorage.setItem("ftpadb-binga", token);
+    }
+</script>
+<?php
+    $TOKEN = explode("ftpadbc=",$_SERVER['REQUEST_URI'])[1];
+    echo $TOKEN;
+?>
 <html lang="pt">
 
 <head>
